eb834e30bec20a2d2c839179be43be2e4035b23e
KernelMrex <24671528+KernelMrex@users.noreply.github.com>
2024-02-11 12:03:03 +0300

    1-ая лаба

--- lab1/task5.hs
+++ lab1/task5.hs
@@ -1,7 +1,5 @@
 -- Список пирамидальных чисел Ферма. Колво чисел = 50
 
-calcNPyramidNumber n = map (\x -> x * x) [0..n]
-
 calcPyramidNumber 1 = 1
 calcPyramidNumber x = x * x + calcPyramidNumber (x - 1)
 
